84971e5b427c8bc98468f3c9ee16e8dd2821954e
Lakshay <englakshaysk@gmail.com>
2022-06-05 18:16:22 +0530

    updated image on admin page

--- src/components/Home/Logo/Logo.js
+++ src/components/Home/Logo/Logo.js
@@ -1,46 +1,13 @@
-import { useEffect, useRef } from 'react'
-import gsap from 'gsap-trial'
-import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
+import React from 'react'
 import LogoS from '../../../assets/images/logo-LLL.png'
 import './Logo.scss'
 
 const Logo = () => {
-   const bgRef = useRef()
-   const outlineLogoRef = useRef()
-   const solidLogoRef = useRef()
-
-   useEffect(() => {
-      gsap.registerPlugin(DrawSVGPlugin)
-
-      gsap
-         .timeline()
-         .to(bgRef.current, {
-            duration: 1,
-            opacity: 1,
-         })
-         .from(outlineLogoRef.current, {
-            drawSVG: 0,
-            duration: 20,
-         })
-
-      gsap.fromTo(
-         solidLogoRef.current,
-         {
-            opacity: 0,
-         },
-         {
-            opacity: 1,
-            delay: 1.5,
-            duration: 4,
-         }
-      )
-   }, [])
-
+  
    return (
-      <div className="logo-container" ref={bgRef}>
+      <div className="logo-container">
          <img
             className="solid-logo"
-            ref={solidLogoRef}
             src={LogoS}
             alt="JavaScript,  Developer"
          />

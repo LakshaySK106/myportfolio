01fa505cb5e96e2ca15346076cf05e3076b745df
Lakshay <90516800+LakshaySK106@users.noreply.github.com>
2023-04-27 18:42:22 +0530

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -40,7 +40,7 @@ function About() {
                </p>
                <div className="myCv">
                      <div className='rtext'>My Resume</div>
-                  <div className='okay'><a href="https://drive.google.com/file/d/1DSiOsbxgFbp8aQzw_JuYmNYrnzZCFsgC/view?usp=sharing" target="__blank">CLICK HERE TO VIEW PDF</a></div>
+                  <div className='okay'><a href="https://drive.google.com/file/d/1fHKgJ1wfQEkTLtRT3oMHFmemGbXnRBaM/view?usp=share_link" target="__blank">CLICK HERE TO VIEW PDF</a></div>
                </div>
             </div>
             
